f922dd525c1a22c82788aa98bbd25be8072257fa
aids61517 <aids61517@gmail.com>
2019-09-19 09:34:32 +0800

    2019/9/18

--- app/src/main/java/com/pinkoi/product/ProductActivity.kt
+++ app/src/main/java/com/pinkoi/product/ProductActivity.kt
@@ -1,9 +1,16 @@
 package com.pinkoi.product
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.pinkoi.product.data.Product
 
 class ProductActivity : AppCompatActivity() {
@@ -18,6 +25,9 @@ class ProductActivity : AppCompatActivity() {
   }
   private val priceText: TextView by lazy {
     findViewById<TextView>(R.id.price_text)
+  }
+  private val recyclerView: RecyclerView by lazy {
+    findViewById<RecyclerView>(R.id.recycler_view)
   }
   private var isFav: Boolean = false
 
@@ -44,5 +54,70 @@ class ProductActivity : AppCompatActivity() {
     nameText.text = product.name
 
     priceText.text = getString(R.string.price, product.price)
+
+
+    recyclerView.apply {
+      layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+
+      adapter = ProductAdapter(context)
+    }
+
+    val productList = mutableListOf<Product>().apply {
+      add(Product("1", 100, R.drawable.product_1))
+      add(Product("2", 200, R.drawable.product_2))
+      add(Product("3", 300, R.drawable.product_3))
+      add(Product("1", 100, R.drawable.product_1))
+      add(Product("2", 200, R.drawable.product_2))
+      add(Product("3", 300, R.drawable.product_3))
+      add(Product("1", 100, R.drawable.product_1))
+      add(Product("2", 200, R.drawable.product_2))
+      add(Product("3", 300, R.drawable.product_3))
+      add(Product("1", 100, R.drawable.product_1))
+      add(Product("2", 200, R.drawable.product_2))
+      add(Product("3", 300, R.drawable.product_3))
+    }
+    (recyclerView.adapter as? ProductAdapter)?.setData(productList)
+  }
+}
+
+private class ProductAdapter(
+  private val context: Context
+): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+  private var productList: List<Product> = emptyList()
+
+  fun setData(productList: List<Product>) {
+    this.productList = productList
+    notifyDataSetChanged()
+  }
+
+  override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+    val productItemView = LayoutInflater.from(context).run {
+      inflate(R.layout.activity_product_item, parent, false)
+    }
+    return ProductItemViewHolder(productItemView)
+  }
+
+  override fun getItemCount(): Int {
+    return productList.size
+  }
+
+  override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+    if (holder is ProductItemViewHolder) {
+      val product = productList[position]
+      holder.nameText.apply {
+        text = product.name
+      }
+
+      holder.productImage.apply {
+        setImageResource(product.imageId)
+      }
+    }
+  }
+
+  class ProductItemViewHolder(
+    itemView: View
+  ): RecyclerView.ViewHolder(itemView) {
+    val productImage: ImageView = itemView.findViewById(R.id.product_image)
+    val nameText: TextView = itemView.findViewById(R.id.product_name_text)
   }
 }
